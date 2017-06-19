5c611ff3f3d03c2eca79e8be4207ca45d242f762
wade <wade@dianhun.cn>
2017-06-19 14:47:15 +0800

    2.spool sub directory

---
+++ src/test/java/TestConstants.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+/**
+ * Created by zjw on 2017/6/19.
+ */
+public class TestConstants {
+
+    public static String FILE = "e:"+ File.separator+"demo.txt";
+
+    public static String POSITION_DIR = "e:";
+
+    public static String SPOOL_DIRECTORY = "e:";
+
+}
---
+++ src/test/java/TestSource.java
@@ -0,0 +1,41 @@
+import com.google.common.collect.Lists;
+import com.zjw.source.FlumeSpoolSubDirectorySource;
+import org.apache.flume.Channel;
+import org.apache.flume.ChannelSelector;
+import org.apache.flume.Context;
+import org.apache.flume.channel.ChannelProcessor;
+import org.apache.flume.channel.MemoryChannel;
+import org.apache.flume.channel.ReplicatingChannelSelector;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by zjw on 2017/6/19.
+ */
+public class TestSource {
+
+    private FlumeSpoolSubDirectorySource source;
+    private Context context;
+    private Channel channel;
+    private ChannelSelector rcs = new ReplicatingChannelSelector();
+
+    @Before
+    public void before() {
+        this.context = new Context();
+        this.context.put("file", TestConstants.FILE);
+        this.context.put("positionDir", TestConstants.POSITION_DIR);
+        this.context.put("spoolDir", TestConstants.SPOOL_DIRECTORY);
+
+        source = new FlumeSpoolSubDirectorySource();
+        channel = new MemoryChannel();
+        rcs.setChannels(Lists.newArrayList(channel));
+        source.setChannelProcessor(new ChannelProcessor(rcs));
+    }
+
+    @Test
+    public void test() {
+        source.configure(context);
+        source.start();
+    }
+
+}
